ff5052d486cb991844bc03862254b9a5f2515d24
timsoriano3 <timsoriano3@gmail.com>
2021-09-12 07:23:40 -0400

    Implemented Role-based Authentication Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,14 +7,26 @@ import { ProfileComponent } from "./user/profile/profile.component";
 import { AdminComponent } from "./admin/admin/admin.component";
 import { NotFoundComponent } from './error/not-found/not-found.component';
 import { UnauthorizedComponent } from './error/unauthorized/unauthorized.component';
+import { AuthGuard } from "./guards/auth.guard";
+import { Role } from "./models/role.enum";
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
-  {path: 'profile', component: ProfileComponent},
-  {path: 'admin', component: AdminComponent},
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    canActivate: [AuthGuard],
+    data: { roles: [Role.ADMIN, Role.USER] }
+  },
+  {
+    path: 'admin',
+    component: AdminComponent,
+    canActivate: [AuthGuard],
+    data: { roles: [Role.ADMIN] }
+  },
   {path: '404', component: NotFoundComponent},
   {path: '401', component: UnauthorizedComponent}
 ];
