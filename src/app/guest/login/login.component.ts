1e24b8f052a422dc66cada64332016e3c52dd05c
timsoriano3 <timsoriano3@gmail.com>
2021-09-12 06:36:55 -0400

    Login Page Completed

--- src/app/guest/login/login.component.ts
+++ src/app/guest/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from "../../models/user.model";
+import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  user: User = new User();
+  faUser = faUserCircle;
+  errorMessage: string = "";
 
-  constructor() { }
+  constructor(private authenticationService: AuthenticationService, private router: Router) { }
 
   ngOnInit(): void {
+    if (this.authenticationService.currentUserValue?.id) {
+      this.router.navigate(['/profile']);
+      return;
+    }
+  }
+
+  login() {
+    this.authenticationService.login(this.user).subscribe(date => {
+      this.router.navigate(['/profile']);
+    }, err => {
+      this.errorMessage = 'Username and/or Password Incorrect!'
+      console.log(err);
+    })
   }
 
 }
