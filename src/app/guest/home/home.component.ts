3d86c8252d0d31197a3d901c62c466a2f0c417a4
timsoriano3 <timsoriano3@gmail.com>
2021-09-17 02:06:52 -0400

    Home Page completed with purchase service and book service methods

--- src/app/guest/home/home.component.ts
+++ src/app/guest/home/home.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { faBook } from '@fortawesome/free-solid-svg-icons';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { BookService } from 'src/app/services/book.service';
+import { PurchaseService } from 'src/app/services/purchase.service';
+import {Book} from "../../models/book.model";
+import {Purchase} from "../../models/purchase.model";
 
 @Component({
   selector: 'app-home',
@@ -7,9 +13,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  bookList: Array<Book> = [];
+  faBook = faBook;
+  errorMessage: string = "";
+  infoMessage: string = "";
 
-  ngOnInit(): void {
+  constructor(private authenticationService: AuthenticationService,
+              private bookService: BookService,
+              private purchaseService: PurchaseService) {
+
   }
 
+  ngOnInit(): void {
+    this.bookService.getAllBooks().subscribe(data => {
+      this.bookList = data;
+    });
+  }
+
+  purchase(item: Book) {
+    if (!this.authenticationService.currentUserValue?.id) {
+      this.errorMessage = 'You should log in to buy a book.';
+      return;
+    }
+
+    const purchase = new Purchase(this.authenticationService.currentUserValue.id, item.id, item.price);
+    this.purchaseService.savePurchase(purchase).subscribe(data => {
+      this.infoMessage = 'Purchase Completed Successfully!'
+    }, err => {
+      this.errorMessage = 'Unexpected error occured.';
+      console.log(err);
+    });
+  }
 }
